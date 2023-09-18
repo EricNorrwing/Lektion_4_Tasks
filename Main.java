6860e22dc15740d80a71d4a0479278836db22d67
EricNorrwing <Eric.Norrwing@gmail.com>
2023-09-18 14:42:12 +0200

    Task #6 stuff

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 package com.EricNorrwing.GitTasks;
 
 public class Main {
-
+ //This comment is a change for task #6
 
 }
--- packagetest/newStuff.java
+++ packagetest/newStuff.java
@@ -3,4 +3,5 @@ package com.EricNorrwing.GitTasks.packagetest;
 public class newStuff {
 
     //This is a change for task #6
+    //And another
 }
